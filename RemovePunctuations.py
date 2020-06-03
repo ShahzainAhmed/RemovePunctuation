084611047c15d7febab37e64000485b4e17a65e1
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-06-04 00:38:13 +0500

    Final Commit

--- RemovePunctuations.py
+++ RemovePunctuations.py
@@ -4,15 +4,14 @@ punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 # Creating a variable.
 my_str = "Hello!!!, he said ---and went."
 
-
 # Incase you want to take input from the user, you can remove it from the comments, and use it to take input.
 # my_str = input("Enter a string: ")
 
-# remove punctuation from the string
+# removing punctuation from the string
 no_punct = ""
 for char in my_str:
    if char not in punctuations:
        no_punct = no_punct + char
 
-# Printing the unpunctuated string
+# Printing the unpunctuated string.
 print(no_punct)
